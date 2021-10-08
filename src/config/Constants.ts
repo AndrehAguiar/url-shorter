54382aa1ecd2ccbf95ffe5a029f11c9622bb852c
AndrehAguiar <andreaguiar.g@gmail.com>
2021-10-08 00:17:22 -0300

    Publish project URL shorter

---
+++ src/config/Constants.ts
@@ -0,0 +1,4 @@
+export const config = {
+    API_URL: 'http://localhost:3000',
+    MONGO_CONNECTION: 'mongodb+srv://andre:testAdmin123@cluster0.guwek.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import { MongoConnection } from './data/MongoConnection';
+import { URLController } from './controller/URLController';
+
+const api = express()
+api.use(express.json())
+
+const database = new MongoConnection()
+database.connect()
+
+const urlController = new URLController()
+
+api.post('/shorten', urlController.shorten)
+api.get('/:hash', urlController.redirect)
+
+api.listen(3000, () => console.log('Express listening'));
